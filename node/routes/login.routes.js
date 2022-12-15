03b4b94496d1cbe82b7ae48a36c54784b82a5ae1
Jerko Segvic <jerko.segvic@gmail.com>
2022-12-15 20:39:44 +0100

    sređen jwt(valjda)

--- node/routes/login.routes.js
+++ node/routes/login.routes.js
@@ -3,26 +3,30 @@ const router = express.Router()
 const path = require('path')
 const db = require('../db')
 const jwt = require('jsonwebtoken');
-const secretKey = "tajnikljuc"
-
-router.post("/", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username == undefined || password == undefined) {
-        res.status(400);
-        res.json({ status: "fail" });
-        return;
-    }
-
-    const users = await db.query(userQuery, [username, password]);
 
 
+const userQuery = 'select * from registered where username = $1 and password = $2 and created is not null';
+
+
+router.post('/', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    if (username == undefined || password == undefined )
+        {
+            res.status(400);
+            res.json({status: "fail"})
+            return
+        }
+
+    const users = await db.query(userQuery,[username, password])
 
     if(users.rowCount == 0) {
         res.status(401)
         res.json({status: 'failed'})
         return
     }
+
+    const secretKey = "tajnikljuc"
     const user = users.rows[0]
     const token = jwt.sign({ id: user.userid, username: user.username, role: "user"}, secretKey, { expiresIn: '24h' })
 
@@ -31,4 +35,5 @@ router.post("/", async (req, res) => {
     return
 })
 
-module.exports = router;
+module.exports = router
+
