16fa83282b4b7df18767e69e135f10592025ef97
InkretIvan <ivaninkret7@gmail.com>
2022-12-17 23:46:49 +0100

    filter fix

--- node/routes/filter.routes.js
+++ node/routes/filter.routes.js
@@ -4,26 +4,24 @@ const path = require('path')
 const db = require('../db')
 
 router.post('/', async (req, res) => {
-    
-
 
     let body = Object();
 
     let programename=req.body.programename
     let abbreviationcourse=req.body.abbreviationcourse
     let categoryname=req.body.categoryname
-    let typeofpost=req.body.typeofpost
+  
 
     console.log(programename)
     console.log(abbreviationcourse)
     console.log(categoryname)
-    console.log(typeofpost)
+  
 
     if(programename==null)  programename='%'
     if(abbreviationcourse==null)  abbreviationcourse='%'
     if(categoryname==null)  categoryname='%'
 
-    let qString = "select post.postid, post.posttitle, post.postdescription, post.timeofcreation, post.creatoruserid, post.categoryid from post natural join category natural join course natural join study_programme where programename like '" + programename + "' and abbreviationcourse like '" + abbreviationcourse + "' and categoryname like '" + categoryname + "'"
+    let qString = "select * from post natural join category natural join course natural join study_programme where programename like '" + programename + "' and abbreviationcourse like '" + abbreviationcourse + "' and categoryname like '" + categoryname + "'"
 
 
     console.log(qString)
